47fdabc899b2581b9dcf14be06cdf1daf153b47b
BigCelz <stanleychilaka22270@gmail.com>
2026-05-27 20:00:03 +0100

    fixes - resturant

--- src/app/components/HOC/Provider.jsx
+++ src/app/components/HOC/Provider.jsx
@@ -1,13 +1,7 @@
 "use client";
-import { ThemeProvider } from "next-themes";
-import React from "react";
 
 const Provider = ({ children }) => {
-  return (
-    <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system">
-      {children}
-    </ThemeProvider>
-  );
+  return children;
 };
 
 export default Provider;
